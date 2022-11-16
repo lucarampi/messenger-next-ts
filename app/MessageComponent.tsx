be890fb3dcdcac9f2e988b2b8574d2890aa069fe
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-16 19:42:44 -0300

    user style

--- app/MessageComponent.tsx
+++ app/MessageComponent.tsx
@@ -6,9 +6,10 @@ interface MessageComponentProps {
 }
 
 export function MessageComponent({ message }: MessageComponentProps) {
+    const isUser = true;
   return (
-    <div className="flex w-fit">
-      <div className="flex-shrink-0">
+    <div className={`flex w-fit ${isUser && 'ml-auto'}`}>
+      <div className={`flex-shrink-0 ${isUser && 'order-2'}`}>
         <Image
           src={message.profilePic}
           alt="Profile image"
@@ -18,12 +19,12 @@ export function MessageComponent({ message }: MessageComponentProps) {
         />
       </div>
       <div>
-        <p className="text-xs px-0.5 pb-0.5 text-red-400">{message.username}</p>
+        <p className={`text-xs px-0.5 pb-0.5  ${isUser ? 'text-blue-400 text-right' : 'text-red-400 text-left'}`}>{message.username}</p>
         <div className="flex items-end">
-          <div className="px-3 py-2 rounded-lg w-fit text-white bg-red-400">
+          <div className={`px-3 py-2 rounded-lg w-fit text-white  ${isUser ? 'bg-blue-400 ml-auto order-2' : 'bg-red-400'}`}>
             <p>{message.message}</p>
           </div>
-          <p className="text-xs italic px-2 text-gray-300">{new Date(message.created_at).toLocaleString()}</p>
+          <p className={`text-xs italic px-2 text-gray-300 text-left ${isUser && 'text-white text-right'}`}>{new Date(message.created_at).toLocaleString()}</p>
         </div>
       </div>
     </div>
