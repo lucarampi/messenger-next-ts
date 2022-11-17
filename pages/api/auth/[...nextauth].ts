f012fbf36c002bf1672c2cee082182050733e267
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-17 17:01:13 -0300

    TEST2

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,7 +9,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  secret:process.env.NEXTAUTH_SECRET!,
+  secret:process.env.NEXT_PUBLIC_SECRET!,
   pages:{
     signIn:'/auth/signin'
   }
