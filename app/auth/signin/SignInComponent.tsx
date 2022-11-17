bb2a1128a2350420df0d66a7eb0cf84453ff5121
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-17 17:35:06 -0300

    test1

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -6,9 +6,10 @@ interface Props {
 }
 
 export default function SignInComponent({ providers }: Props) {
+  
   return (
     <div>
-      {Object.values(providers!).map((provider) => (
+      {(Object.values(providers!)|| 'error').map((provider) => (
         <div key={provider.name}>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all"
