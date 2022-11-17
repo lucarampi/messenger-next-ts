d15b4ff4d871f46c470aa8476e1d4db165f80b63
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-17 16:34:20 -0300

    session server side

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ export default async function HomePage() {
   const {data} = await axiosClient.get("/api/getMessages");
   const messages: Message[] = data.messages;
 
-  // const session = await unstable_getServerSession();
+  const session = await unstable_getServerSession();
   
-  const session = {
-    user:{
-      email:'temp@mail.com',
-      image:'',
-      name:'Temp Name'
+  // const session = {
+  //   user:{
+  //     email:'temp@mail.com',
+  //     image:'',
+  //     name:'Temp Name'
       
-    }
-  } as  Awaited<ReturnType<typeof unstable_getServerSession>>
+  //   }
+  // } as  Awaited<ReturnType<typeof unstable_getServerSession>>
 
   return (
     <Provider session={session}>
