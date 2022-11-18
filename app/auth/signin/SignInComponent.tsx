6465dc5d00eec75fcb293fbfcca166aec3f74e4a
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-18 16:39:42 -0300

    test

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -9,14 +9,17 @@ export default function SignInComponent({ providers }: Props) {
   const error = [{ name: "error", id: "error" }];
   // const {data} = useSession()
   // console.log('DATA FROM SIGNIN COMPONENT >>>',data)
+
   return (
     <div>
       {Object.values(providers! || error).map((provider) => (
         <div key={provider.name}>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all"
-            onClick={() => {signIn(provider.id,{
-              callbackUrl:'/'
+            onClick={() => {
+              
+              signIn(provider.id,{
+              callbackUrl:provider.callbackUrl
             });
             }}
           >
