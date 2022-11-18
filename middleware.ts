2a392b4d99997be09618cccc967b66313ad7d28c
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-18 19:33:15 -0300

    fiz redirect after login

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,25 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher: ["/"],
+    matcher: ["/"]
 }
+
+// import { withAuth } from "next-auth/middleware"
+
+// export default withAuth(
+//   // `withAuth` augments your `Request` with the user's token.
+//   function middleware(req) {
+//     console.log(req.nextauth.token)
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => !!token,
+//     },
+    
+//   }
+// )
+
+
+// export const config = {
+//     matcher: ["/"],
+// }
