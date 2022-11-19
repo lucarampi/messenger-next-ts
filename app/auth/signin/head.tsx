d9365d93ffddf43406624ee8003a2470cb46adcf
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-19 03:31:58 -0300

    login favicon

--- app/auth/signin/head.tsx
+++ app/auth/signin/head.tsx
@@ -1,7 +1,9 @@
-export default function Head(){
-  return(
-    <title>
-        Sign in to Earth Messenger
-    </title>
+export default function Head() {
+  return (
+    <>
+      <title>Sign in to Earth Messenger</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/logo-messenger.png" />
+    </>
   );
 }
