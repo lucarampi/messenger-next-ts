7500b3d37553ad72df81a7f8f7cff5f3aca23c6d
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-19 03:36:40 -0300

    removin logo

--- app/Header.tsx
+++ app/Header.tsx
@@ -38,7 +38,6 @@ export default async function Header() {
     <header className="flex justify-center items-center px-10 py-7 shadow-md shadow-slate-200 sticky top-0 z-50 bg-white lex">
       <div className="flex flex-col items-center space-y-5">
         <div className="flex space-x-2 items-center">
-          <Image className=" " src="/logo-meta.png" alt="logo" height={10} width={50} />
           <p className="text-blue-500">Welcome to Earth Messenger</p>
         </div>
       </div>
