b69f76369045265e8a6928ccd4b7eaf9fe735f5e
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-19 17:27:08 -0300

    added github image provider to config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["platform-lookaside.fbsbx.com",'scontent-gru1-2.xx.fbcdn.net',"lh3.googleusercontent.com"]
+    domains: ["platform-lookaside.fbsbx.com",'scontent-gru1-2.xx.fbcdn.net',"lh3.googleusercontent.com","avatars.githubusercontent.com"]
   },
   experimental: {
     appDir: true
