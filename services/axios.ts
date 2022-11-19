690c6ef522ff26b8eff0b21fb598cf684e9891c2
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-19 00:59:13 -0300

    teste no https

--- services/axios.ts
+++ services/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosClient = axios.create({
-    baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    baseURL: `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 }) 
